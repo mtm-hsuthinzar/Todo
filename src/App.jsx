35e234f4f28380ccb3db892f589af06a0514ac0b
Hsu Thinzar <hsumeow1210@gmail.com>
2024-05-26 22:06:08 +0630

    API updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,138 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import {
-	Box,
-	IconButton,
-	List,
-	OutlinedInput,
-	InputAdornment,
-} from "@mui/material";
+  Box,
+  IconButton,
+  List,
+  OutlinedInput,
+  InputAdornment,
+  Alert,
+} from "@mui/material"; // Import from Material UI
 
-import { Add as AddIcon } from "@mui/icons-material";
+import { Add as AddIcon } from "@mui/icons-material"; // Import Icon
 
 import Item from "./Item";
 import Header from "./Header";
 
+const api = "http://localhost:8080/tasks";
+
 export default function App() {
-	const inputRef = useRef();
+  const inputRef = useRef();
+  const [data, setData] = useState([]);
+  const [IsLoading, setIsLoading] = useState(true);
+  const [hasError, sethasError] = useState(false);
 
-	const [data, setData] = useState([
-		{ id: 1, name: "Apple", done: true },
-		{ id: 2, name: "Orange", done: false },
-		{ id: 3, name: "Milk", done: false },
-	]);
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch(api);
+        if (res.ok) {
+          setData(await res.json());
+          setIsLoading(false);
+        } else {
+          sethasError(true);
+        }
+      } catch (e) {
+        sethasError(false);
+      }
+    })();
+  }, []);
 
-	const remove = id => {
-		setData(data.filter(item => item.id !== id));
-	};
+  const remove = (_id) => {
+    setData(data.filter((item) => item._id !== _id));
+    fetch(`${api}/${_id}`, { method: "DELETE" });
+  };
 
-	const toggle = id => {
-		setData(
-			data.map(item => {
-				if (item.id === id) item.done = !item.done;
-				return item;
-			})
-		);
-	};
+  const toggle = (_id) => {
+    setData(
+      data.map((item) => {
+        if (item._id === _id) item.done = !item.done;
+        return item;
+      })
+    );
 
-	const add = name => {
-		const id = data[data.length - 1].id + 1;
-		setData([...data, { id, name, done: false }]);
-	};
+    fetch(`${api}/${_id}/toggle`, { method: "PUT" });
+  };
 
-	return (
-		<Box>
-            <Header count={data.filter(item => !item.done).length} />
-			<Box sx={{ mx: "auto", maxWidth: "md", mt: 4 }}>
-				<form
-					onSubmit={e => {
-						e.preventDefault();
+  const add = async (name) => {
+    const res = await fetch(api, {
+      method: "POST",
+      body: JSON.stringify({ name }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const item = await res.json();
+    const _id = data[data.length - 1]._id + 1;
+    setData([...data, item]);
+  };
 
-						const name = inputRef.current.value;
-						if (!name) return false;
+  return (
+    <Box>
+      <Header count={data.filter((item) => !item.done).length} />
+      <Box sx={{ mx: "auto", maxWidth: "md", mt: 4 }}>
+        {IsLoading && <Box sx={{ mb: 2, textAlign: "center" }}>Loading...</Box>}
 
-						add(name);
+        {hasError && (
+          <Alert severity="error" sx={{ mb: 2 }}>
+            Something went wrong
+          </Alert>
+        )}
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
 
-						inputRef.current.value = "";
-						inputRef.current.focus();
-					}}>
-					<OutlinedInput
-						fullWidth
-						inputRef={inputRef}
-						endAdornment={
-							<InputAdornment>
-								<IconButton type="submit">
-									<AddIcon />
-								</IconButton>
-							</InputAdornment>
-						}
-					/>
-				</form>
-				<List>
-					{data
-						.filter(item => !item.done)
-						.map(item => {
-							return (
-								<Item
-									key={item.id}
-									item={item}
-									remove={remove}
-									toggle={toggle}
-								/>
-							);
-						})}
-				</List>
-				<hr />
-				<List>
-					{data
-						.filter(item => item.done)
-						.map(item => {
-							return (
-								<Item
-									key={item.id}
-									item={item}
-									remove={remove}
-									toggle={toggle}
-								/>
-							);
-						})}
-				</List>
-			</Box>
-		</Box>
-	);
+            const name = inputRef.current.value;
+            if (!name) return false;
+
+            add(name);
+
+            inputRef.current.value = "";
+            inputRef.current.focus();
+          }}
+        >
+          <OutlinedInput
+            fullWidth
+            inputRef={inputRef}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton type="submit">
+                  <AddIcon />
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </form>
+        <List>
+          {data
+            .filter((item) => !item.done)
+            .map((item) => {
+              return (
+                <Item
+                  key={item._id}
+                  item={item}
+                  remove={remove}
+                  toggle={toggle}
+                />
+              );
+            })}
+        </List>
+        <hr />
+        <List>
+          {data
+            .filter((item) => item.done)
+            .map((item) => {
+              return (
+                <Item
+                  key={item._id}
+                  item={item}
+                  remove={remove}
+                  toggle={toggle}
+                />
+              );
+            })}
+        </List>
+      </Box>
+    </Box>
+  );
 }
