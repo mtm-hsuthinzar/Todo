658e23239c37943836a89b4b9913176d80d0b1b3
Hsu Thinzar <mtm.hsuthinzar@gmail.com>
2025-03-03 09:41:26 +0630

    I

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,8 @@ export default function Footer() {
     <footer>
       <h2>This is footer</h2>
       <nav>
-        This is footer navigation
+        This is footer navigation.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad officiis perferendis distinctio dolore dicta nihil dolor est, impedit aliquam, et ab cupiditate libero assumenda alias quis eveniet nam incidunt illo?
       </nav>
     </footer>
   )
